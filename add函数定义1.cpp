d8593a02df0ecedaf1015d4420ca87a00a0e1f46
RevxZxCx <111618558+RevxZxCx@users.noreply.github.com>
2022-09-22 09:41:49 +0800

    Add files via upload

---
+++ add函数定义1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int add(int x, int y)
+
+{
+	int z = x + y;
+	return z;
+}
+
+
+
+int mian()
+{
+	num1 = 2004;
+	num2 = 520;
+	gogogo = add(num1, num2);
+	printf("%d", gogogo);
+	return 0;
+}
