548d4dc0382a00faede2da65c22142923c51ca99
RevxZxCx <111618558+RevxZxCx@users.noreply.github.com>
2022-09-23 16:01:42 +0800

    Add files via upload

---
+++ 视觉（小把戏）.cpp
@@ -0,0 +1,14 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int i =0;
+//	for (i = 0; 1; i++)
+//	{
+//		printf("²Ý");
+//		if (i % 60 == 0)
+//			printf("\n");
+//		else if (i % 58 == 0)
+//			printf("\n");
+//	}
+//	return 0;
+//}
